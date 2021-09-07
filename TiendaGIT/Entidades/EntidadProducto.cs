b3dce37284b867fa8e38490eed0af08d00401579
Franco <carlosfranciscoherrerafranco@gmail.com>
2021-09-07 10:38:02 -0500

    V1.0 Para Entidades

---
+++ TiendaGIT/Entidades/EntidadProducto.cs
@@ -0,0 +1,18 @@
+﻿namespace Entidades
+{
+    public class EntidadProducto
+    {
+        public EntidadProducto(int idProducto, string nombre, double precio, string descripcion)
+        {
+            _idProducto = idProducto;
+            _Nombre = nombre;
+            _Precio = precio;
+            _Descripcion = descripcion;
+        }
+
+        public int _idProducto { get; set; }
+        public string _Nombre { get; set; }
+        public double _Precio { get; set; }
+        public string _Descripcion { get; set; }
+    }
+}
