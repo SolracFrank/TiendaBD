de7a548a96b6ff533ff41bc354ee2eb0ef88747e
Franco <carlosfranciscoherrerafranco@gmail.com>
2021-09-07 10:43:05 -0500

    V1.1 Creación de Manejadores

---
+++ TiendaGIT/Manejadores/ManejadorProducto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using DataAccess;
+
+namespace Manejadores
+{
+    public class ManejadorProducto
+    {
+        Base _base;
+        public ManejadorProducto()
+        {
+            _base = new Base("localhost","root","","Tienda",3306);
+        }
+
+
+
+
+    }
+}
